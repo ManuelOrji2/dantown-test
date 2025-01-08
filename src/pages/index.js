173b800025287f7e8309e823f2a671d9ced4c4a2
Orji Emmanuel Iheanacho <nuelorji98@gmail.com>
2025-01-08 17:14:22 +0100

    Update: built table for users ranking

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,23 @@ import mamCircleIcon from "../../public/images/Mask Group.png";
 import crownIcon from "../../public/images/Group 131.png";
 import skaterCircleIcon from "../../public/images/Ellipse 5.png";
 
-// import {
-// 	Table,
-// 	TableHeader,
-// 	TableColumn,
-// 	TableBody,
-// 	TableRow,
-// 	TableCell,
-// } from "@nextui-org/react";
+import AriaCircleIcon from "../../public/images/Mask Group (1).png";
+import cyclistCircleIcon from "../../public/images/Ellipse 5 (1).png";
+
+import trentCircleIcon from "../../public/images/Mask Group (2).png";
+import skater2CircleIcon from "../../public/images/Ellipse 5 (2).png";
+
+import shakiraCircleIcon from "../../public/images/Mask Group (3).png";
+import yogaCircleIcon from "../../public/images/Ellipse 5 (3).png";
+
+import {
+	Table,
+	TableHeader,
+	TableColumn,
+	TableBody,
+	TableRow,
+	TableCell,
+} from "@nextui-org/react";
 
 const sportsContents = [
 	{
@@ -60,7 +69,7 @@ export default function Home() {
 					<div className="w-[320px] p-[10px] flex items-center gap-4 bg-white rounded-[20px]">
 						<Image src={searchIcon} alt="" />
 						<input
-							className="outline-none"
+							className="outline-none placeholder:text-[14px]"
 							type="text"
 							placeholder="Search here"
 						/>
@@ -83,42 +92,127 @@ export default function Home() {
 						</div>
 					))}
 				</div>
-				<div className="w-full h-[390px] mt-[30px] bg-white rounded-[20px] p-[24px]">
-					<div className="flex items-center justify-between">
+				<div className="w-full h-[390px] flex flex-col items-center mt-[30px] bg-white rounded-[20px] p-[24px]">
+					<div className="w-full flex items-center justify-between p-[10px]">
 						<h1 className="text-[#0F3F62] text-[16px] font-bold">Friends</h1>
 						<Image src={doubleDots} alt="" />
 					</div>
+					<Table
+						css={{
+							borderCollapse: "separate",
+							borderSpacing: "30px 0",
+							gap: "40px", // Adds gap between rows
+						}}
+					>
+						<TableHeader>
+							<TableColumn className="text-[#8695A0] text-[14px] font-medium">
+								Rank
+							</TableColumn>
+							<TableColumn className="text-[#8695A0] text-[14px] font-medium">
+								Name
+							</TableColumn>
+							<TableColumn className="text-[#8695A0] text-[14px] font-medium">
+								Category
+							</TableColumn>
+							<TableColumn className="text-[#8695A0] text-[14px] font-medium">
+								Points
+							</TableColumn>
+						</TableHeader>
+						<TableBody>
+							<TableRow className="border-spacing-4" key="1">
+								<TableCell>
+									<div className="flex items-center">
+										<Image src={numberOneIcon} alt="" />
+									</div>
+								</TableCell>
+								<TableCell className="flex items-center gap-5">
+									<div className="flex items-center gap-4">
+										<Image src={mamCircleIcon} />
+										<span className="text-[16px] text-[#0F3F62] font-bold">
+											Mamang Ironmen
+										</span>
+									</div>
+									<Image src={crownIcon} />
+								</TableCell>
+								<TableCell>
+									<div className="w-[102px] bg-[] flex items-center gap-2">
+										<Image src={skaterCircleIcon} alt="" />
+										<span className="text-[14px] text-[#0F3F62]">Skaters</span>
+									</div>
+								</TableCell>
+								<TableCell className="text-[16px] text-[#0F3F62]">
+									2.140
+								</TableCell>
+							</TableRow>
+							<TableRow key="2">
+								<TableCell>
+									<span className="text-center px-[9px]">2</span>
+								</TableCell>
+								<TableCell className="flex items-center gap-5">
+									<div className="flex items-center gap-4">
+										<Image src={AriaCircleIcon} />
+										<span className="text-[16px] text-[#0F3F62] font-bold">
+											Ariana Grande
+										</span>
+									</div>
+								</TableCell>
+								<TableCell>
+									<div className="w-[102px] bg-[] flex items-center gap-2">
+										<Image src={cyclistCircleIcon} alt="" />
+										<span className="text-[14px] text-[#0F3F62]">Cyclist</span>
+									</div>
+								</TableCell>
+								<TableCell className="text-[16px] text-[#0F3F62]">
+									1.814
+								</TableCell>
+							</TableRow>
+							<TableRow key="3">
+								<TableCell>
+									<span className="text-center px-[9px]">3</span>
+								</TableCell>
+								<TableCell className="flex items-center gap-5">
+									<div className="flex items-center gap-4">
+										<Image src={trentCircleIcon} />
+										<span className="text-[16px] text-[#0F3F62] font-bold">
+											Trent Alexander Arnold
+										</span>
+									</div>
+								</TableCell>
+								<TableCell>
+									<div className="w-[102px] bg-[] flex items-center gap-2">
+										<Image src={skater2CircleIcon} alt="" />
+										<span className="text-[14px] text-[#0F3F62]">Skaters</span>
+									</div>
+								</TableCell>
+								<TableCell className="text-[16px] text-[#0F3F62]">
+									1.620
+								</TableCell>
+							</TableRow>
+							<TableRow key="4">
+								<TableCell>
+									<span className="text-center px-[9px]">4</span>
+								</TableCell>
+								<TableCell className="flex items-center gap-5">
+									<div className="flex items-center gap-4">
+										<Image src={shakiraCircleIcon} />
+										<span className="text-[16px] text-[#0F3F62] font-bold">
+											Shakira Wakawaka
+										</span>
+									</div>
+								</TableCell>
+								<TableCell>
+									<div className="w-[102px] bg-[] flex items-center gap-2">
+										<Image src={yogaCircleIcon} alt="" />
+										<span className="text-[14px] text-[#0F3F62]">Yoga</span>
+									</div>
+								</TableCell>
+								<TableCell className="text-[16px] text-[#0F3F62]">
+									1.280
+								</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
 				</div>
-				{/* 
-				<Table hideHeader aria-label="Example static collection table">
-					<TableHeader>
-						<TableColumn>NAME</TableColumn>
-						<TableColumn>ROLE</TableColumn>
-						<TableColumn>STATUS</TableColumn>
-					</TableHeader>
-					<TableBody>
-						<TableRow key="1">
-							<TableCell>Tony Reichert</TableCell>
-							<TableCell>CEO</TableCell>
-							<TableCell>Active</TableCell>
-						</TableRow>
-						<TableRow key="2">
-							<TableCell>Zoey Lang</TableCell>
-							<TableCell>Technical Lead</TableCell>
-							<TableCell>Paused</TableCell>
-						</TableRow>
-						<TableRow key="3">
-							<TableCell>Jane Fisher</TableCell>
-							<TableCell>Senior Developer</TableCell>
-							<TableCell>Active</TableCell>
-						</TableRow>
-						<TableRow key="4">
-							<TableCell>William Howard</TableCell>
-							<TableCell>Community Manager</TableCell>
-							<TableCell>Vacation</TableCell>
-						</TableRow>
-					</TableBody>
-				</Table> */}
 			</section>
 			<div className="flex items-center">
 				<Image className="" src={verticalStroke} alt="" />
